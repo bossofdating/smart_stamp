b6a134859b74238e718cfe6b6e1f68a0e030c65d
kyuil91 <kyuil91@gmail.com>
2020-09-14 17:37:36 +0900

    리스트 페이징 / 리싸이클러뷰 적용

---
+++ app/src/main/java/com/kt/SmartStamp/adapter/RecyclerViewAdapterMainDashboard.java
@@ -0,0 +1,106 @@
+package com.kt.SmartStamp.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.kt.SmartStamp.R;
+import com.kt.SmartStamp.data.ServerDataContract;
+import com.kt.SmartStamp.listener.LIST_ITEM_LISTENER;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+public class RecyclerViewAdapterMainDashboard extends RecyclerView.Adapter<RecyclerViewAdapterMainDashboard.ViewHolder> implements View.OnClickListener {
+	/******************************************* ViewHolder *******************************************/
+	class ViewHolder extends RecyclerView.ViewHolder {
+		public TextView cont_name_textview;
+		public TextView cont_state_textview;
+		public TextView cont_date_textview;
+		public TextView cont_detail_textview;
+
+
+		ViewHolder(View view) {
+			super(view);
+			// 레이아웃 연결
+			cont_name_textview = view.findViewById( R.id.cont_name_textview );
+			cont_state_textview = view.findViewById( R.id.cont_state_textview );
+			cont_date_textview = view.findViewById( R.id.cont_date_textview );
+			cont_detail_textview = view.findViewById( R.id.cont_detail_textview );
+		}
+	}
+
+	/*************************************** 클래스 전역변수 영역 *************************************/
+	private ArrayList<ServerDataContract> contractArrayList;
+	private LayoutInflater layoutInflater;
+	private Context context;
+
+	/******************************************** 생성자 **********************************************/
+	public RecyclerViewAdapterMainDashboard(Context context, ArrayList<ServerDataContract> contractArrayList, LIST_ITEM_LISTENER EventListener) {
+		// 외부변수 연결
+		this.context = context;
+		this.contractArrayList = contractArrayList;
+
+		// 인스턴스 생성
+		layoutInflater = (LayoutInflater) context.getSystemService( Context.LAYOUT_INFLATER_SERVICE );
+	}
+
+	/*********************************** OnAttachedToRecyclerView **********************************/
+	@Override
+	public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+		super.onAttachedToRecyclerView(recyclerView);
+	}
+
+	/********************************* OnDetachedFromRecyclerView ********************************/
+	@Override
+	public void onDetachedFromRecyclerView(RecyclerView recyclerView) {
+		super.onDetachedFromRecyclerView(recyclerView);
+	}
+
+	/************************************** OnCreateViewHolder **************************************/
+	@Override
+	public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+		View convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.sublayout_listview_main_dashiboard, parent, false);
+		convertView.setOnClickListener(this);
+		return new ViewHolder(convertView);
+	}
+	/*************************************** OnBindViewHolder ***************************************/
+	@Override
+	public void onBindViewHolder(@NonNull ViewHolder Holder, int position ) {
+		// 레이아웃 출력
+		ServerDataContract serverDataContract = contractArrayList.get(position);
+		Holder.cont_name_textview.setText(serverDataContract.cont_name);
+		Holder.cont_date_textview.setText("반출 기간 : " + serverDataContract.appr_st_dt + " ~ " + serverDataContract.appr_st_dt);
+		Holder.cont_detail_textview.setText(serverDataContract.cont_detail);
+
+		int doc_after_cnt = Integer.parseInt(serverDataContract.doc_after_cnt);
+		if (doc_after_cnt > 0) {
+			Holder.cont_state_textview.setText("등록중 (" + serverDataContract.doc_after_cnt + ")");
+			Holder.cont_state_textview.setTextColor(context.getResources().getColor(R.color.colorAccent));
+		} else Holder.cont_state_textview.setText("등록 대기");
+	}
+	/**************************************** OnViewRecycled ****************************************/
+	@Override
+	public void onViewRecycled(ViewHolder Holder) {
+		super.onViewRecycled(Holder);
+	}
+	/******************************************** GetCount ********************************************/
+	@Override
+	public int getItemCount() {
+		return contractArrayList.size();
+	}
+
+	/************************************** 클릭 이벤트 핸들러 ****************************************/
+	@Override
+	public void onClick(View view) {
+	}
+}
